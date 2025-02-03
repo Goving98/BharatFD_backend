2c08abb56e8a38d5c925bdc4f19691068364def1
Goving98 <govinddaliya99@gmail.com>
2025-02-03 19:49:51 +0530

    Initialized the repo

--- faq_project/urls.py
+++ faq_project/urls.py
@@ -4,6 +4,6 @@ from faq.views import FAQListView, home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),  # Home page for FAQs
+    path('', home, name='home'),
     path('api/faqs/', FAQListView.as_view(), name="faq-list"),
 ]
